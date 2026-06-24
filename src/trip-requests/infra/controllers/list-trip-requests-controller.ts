ee87d4069ef7cf727721272a4e969cd4db7fe14e
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-06-24 11:30:07 -0300

    feat(controller): add ListTripRequestsController for handling trip request retrieval

---
+++ src/trip-requests/infra/controllers/list-trip-requests-controller.ts
@@ -0,0 +1,19 @@
+import { BaseController } from './controller.js'
+import { ok, type HttpResponse, type SuccessResponse } from '../../../shared/infra/http/http.js'
+import type { TripRequestOutput } from '../../application/dtos/trip-request-output.js'
+
+interface ListTripRequestsUseCase {
+  execute(): Promise<TripRequestOutput[]>
+}
+
+export class ListTripRequestsController extends BaseController<undefined, TripRequestOutput[]> {
+  constructor(private readonly listTripRequestsUseCase: ListTripRequestsUseCase) {
+    super()
+  }
+
+  protected async perform(): Promise<HttpResponse<SuccessResponse<TripRequestOutput[]>>> {
+    const tripRequests = await this.listTripRequestsUseCase.execute()
+
+    return ok(tripRequests)
+  }
+}
